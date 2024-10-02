6e337cee5d3ad2f0182bc0adf0c21f39c94ae878
Saara Darakshan <saaradarakshan56@gmail.com>
2024-10-02 10:00:11 +0530

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,11 @@
+# Created a Empty List
+stack = []
+
+# Pushing the elements in the list
+stack.append(1)
+stack.append(2)
+stack.append(3)
+
+print(&quot;The Stack items are :&quot;)
+for i in stack:
+    print(i)
